c9ff2ebcc0b0333362d5f833d98aad687d430716
zeynepyagci <zeynepyagci92@gmail.com>
2022-08-09 19:11:44 +0300

    first commit

---
+++ app/src/main/java/com/example/myloginapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myloginapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView btn = findViewById(R.id.SignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/myloginapplication/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.myloginapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView btn = findViewById(R.id.login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
