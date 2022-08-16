87f00af70aa266a8d612cd06150a22fde696e071
zeynepyagci <zeynepyagci92@gmail.com>
2022-08-17 01:32:26 +0300

    fifth commit

--- app/src/main/java/com/example/myloginapplication/Menu.java
+++ app/src/main/java/com/example/myloginapplication/Menu.java
@@ -1,6 +1,9 @@
 package com.example.myloginapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,16 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        TextView button = findViewById(R.id.profile);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Menu.this, Profile.class));
+            }
+        });
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/myloginapplication/Profile.java
@@ -0,0 +1,34 @@
+package com.example.myloginapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity {
+
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout=findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(Profile.this, "Successfully Logout", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
